56ec283a3e0376ef7f966314c82c1c70f0d58c05
Sukhsimrankaur <sukhsimrankaur@academic.rrc.ca>
2025-04-21 20:01:53 -0500

    Try to do WYSIWYG editor

--- edit.php
+++ edit.php
@@ -79,6 +79,17 @@ $categories = $db->query($query)->fetchAll(PDO::FETCH_ASSOC);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="main.css">
+     <!-- TinyMCE WYSIWYG CDN -->
+    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+    <script>
+        tinymce.init({
+            selector: 'textarea#wysiwyg', // We'll give textareas this class
+            height: 300,
+            menubar: false,
+            plugins: 'lists link image preview code',
+            toolbar: 'undo redo | styles | bold italic underline | alignleft aligncenter alignright | bullist numlist outdent indent | link image | code preview'
+        });
+    </script>
 
     <title>Edit Superhero Profile</title>
 </head>
@@ -90,25 +101,25 @@ $categories = $db->query($query)->fetchAll(PDO::FETCH_ASSOC);
     </nav>
     <form method="POST">
         <label for="name">Name</label>
-        <input type="text" name="name" id="name" value="<?= htmlspecialchars($hero['name']) ?>" required>
+        <input type="text" name="name" id="name" class="wysiwyg" value="<?= htmlspecialchars($hero['name']) ?>" required>
 
         <label for="alias">Alias</label>
-        <input type="text" name="alias" id="alias" value="<?= htmlspecialchars($hero['alias']) ?>">
+        <input type="text" name="alias" id="alias" class="wysiwyg" value="<?= htmlspecialchars($hero['alias']) ?>">
 
         <label for="bio">Bio</label>
-        <textarea name="bio" id="bio" required><?= htmlspecialchars($hero['bio']) ?></textarea>
+        <textarea name="bio" id="bio" class="wysiwyg" required><?= htmlspecialchars($hero['bio']) ?></textarea>
 
         <label for="powers">Powers</label>
-        <textarea name="powers" id="powers" required><?= htmlspecialchars($hero['powers']) ?></textarea>
+        <textarea name="powers" id="powers" class="wysiwyg" required><?= htmlspecialchars($hero['powers']) ?></textarea>
 
         <label for="image_url">Image URL</label>
-        <input type="text" name="image_url" id="image_url" value="<?= htmlspecialchars($hero['image_url']) ?>">
+        <input type="text" name="image_url" id="image_url" class="wysiwyg" value="<?= htmlspecialchars($hero['image_url']) ?>">
 
         <label for="affiliation">Affiliation</label>
-        <input type="text" name="affiliation" id="affiliation" value="<?= htmlspecialchars($hero['affiliation']) ?>">
+        <input type="text" name="affiliation" id="affiliation"  class="wysiwyg"value="<?= htmlspecialchars($hero['affiliation']) ?>">
 
         <label for="category_id">Category</label>
-        <select name="category_id" id="category_id">
+        <select name="category_id" id="category_id" class="wysiwyg">
             <?php foreach ($categories as $category): ?>
                 <option value="<?= $category['category_id'] ?>" <?= ($hero['category_id'] == $category['category_id']) ? 'selected' : '' ?>>
                     <?= htmlspecialchars($category['category_name']) ?>
